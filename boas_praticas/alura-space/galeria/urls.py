c044997217e531a1fe4bc2942204f963b4998bc4
Andre Cabalo <andreccabalo@gmail.com>
2024-03-20 21:40:37 -0300

    Adicionando e removendo conteudos

--- boas_praticas/alura-space/galeria/urls.py
+++ boas_praticas/alura-space/galeria/urls.py
@@ -1,8 +1,9 @@
 # Iremos importar:
 from django.urls import path
-from galeria.views import index
+from galeria.views import index,imagem
 
 # Criaremos um lista de urls, que a importaremos la para setup urls.py
 urlpatterns = [
-    path('',index)
+    path('',index, name='index'),
+    path('imagem/',imagem, name='imagem')
 ]
--- boas_praticas/alura-space/galeria/views.py
+++ boas_praticas/alura-space/galeria/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # 2- Criamos um função que faz com que aparece o comando htlm com Alura Space 
 def index(request):
-    return render(request, 'index.html') #usando sempre o render, tendo o primeiro parametro sempre request, apontamos o nome do arquivo
+    return render(request, 'galeria/index.html') #usando sempre o render, tendo o primeiro parametro sempre request, apontamos o nome do arquivo
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html') 
